1c6be8fe0e6b9816dc718523a2a9d0b4ac8c8285
Patrick Lanzinger <patrick.lanzinger@student.uibk.ac.at>
2019-12-09 16:55:50 +0100

    fix makefile

--- proseminar/09/n-queen_mp.c
+++ proseminar/09/n-queen_mp.c
@@ -13,7 +13,7 @@ int nqueens(int n)
   // The top level is two fors, to save one bit of symmetry in the enumeration by forcing second queen to
   // be AFTER the first queen.
   //
-  #pragma omp parallel for schedule(guided) shared(num) private(cols,diagl,diagr,posibs) default(none) 
+  #pragma omp parallel for schedule(guided) shared(num) private(cols,diagl,diagr,posibs,n) default(none) 
   for (int q0=0; q0<n-2; q0++) {
     for (int q1=q0+2; q1<n; q1++){
       int bit0 = 1<<q0;
