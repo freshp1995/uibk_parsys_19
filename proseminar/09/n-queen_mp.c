80c7a26084f54a7b0053c680f57e61aba9775576
Patrick Lanzinger <patrick.lanzinger@student.uibk.ac.at>
2019-12-10 12:34:47 +0100

    not really an improvment

--- proseminar/09/n-queen_mp.c
+++ proseminar/09/n-queen_mp.c
@@ -16,6 +16,7 @@ int nqueens(int n)
   // The top level is two fors, to save one bit of symmetry in the enumeration by forcing second queen to
   // be AFTER the first queen.
   //
+  //#pragma omp simd aligned(cols, diagl, diagr, posibs:32) 
   #pragma omp parallel for schedule(guided) shared(num, n) private(cols,diagl,diagr,posibs) default(none) 
   for (int q0=0; q0<n-2; q0++) {
     for (int q1=q0+2; q1<n; q1++){
